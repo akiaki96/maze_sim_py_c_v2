6708834dcf88b752e0230d3faa3bc20ab3711548
akiaki96 <akinabe1031@gmail.com>
2026-06-13 08:21:28 +0900

    dijekstr 前準備

--- solver/solver_time_based_dijekstra.c
+++ solver/solver_time_based_dijekstra.c
@@ -1,10 +1,18 @@
 
 #include "global.h"
 
-uint8_vector time_based_dijekstra(bool left, bool front, bool right) {
+uint8_vector solver_time_based_dijekstra_init(void) {
     uint8_vector_init(&action_queue);
 
-    
+    uint8_vector_push(&action_queue, ACT_MOVE_FIRST_HALF_CELL);
+
+
+    uint8_vector_push(&action_queue, ACT_FINISH);
+    return action_queue;
+}
+
+uint8_vector solver_time_based_dijekstra(bool left, bool front, bool right) {
+    uint8_vector_init(&action_queue);
 
     return action_queue;
 }
